e741b7cf91d1dfac2cc8fff34233dcc7f52a684b
LoreWebDev24 <lorenzo.delre12@gmail.com>
2023-10-10 10:28:30 +0200

    EMAILS: alerts are now outside of FOR cycle

--- js/master.js
+++ js/master.js
@@ -3,24 +3,27 @@
 
 // ESERCIZIO DI VERIFICA DI UN' EMAIL ALL' INTENRO DI UN ARRAY
 
-let emailsArray = ['lorenzo.delre12@gmail.com', 'fizzassassin@gmail.com', 'buzmelee@gmail.com', 'fizzbuzz@gmail.com']
+let emailsArray = ['lorenzo.delre12@gmail.com', 'fizzassassin@gmail.com', 'buzmelee@gmail.com', 'fizzbuzz@gmail.com'];
 
-let userEmailPrompt = prompt('Inserisci la tua email')
+let userEmailPrompt = prompt('Inserisci la tua email');
 
-const lunghezzaArray = emailsArray.length
+const lunghezzaArray = emailsArray.length;
+
+let validation = false;
 
 for (let i = 0; i < lunghezzaArray; i++) {
-	const  emailCorrente = emailsArray[i]
-	console.log(i, emailCorrente)
-
-    if (userEmailPrompt != emailsArray[i]) {
-        alert('email non valida / ricarica la pagina')
-    }
-
-    else {
-        alert('benvenuto nel nostro sito')
+	const  emailCorrente = emailsArray[i];
+	console.log(i, emailCorrente);
+    if (userEmailPrompt === emailCorrente) {
+        validation = true;
     }
 }
 
+if (validation === true) {
+    alert('benvenuto nel nostro sito');
+    
+}
 
-
+else {
+    alert('email non valida / ricarica la pagina');
+}
